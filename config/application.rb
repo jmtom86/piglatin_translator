3f56a8ed4a6dc8cb07dd6fe78673f550374802ed
Jordan <jmtom86@yahoo.com>
2020-02-17 23:34:32 -0800

    updated Gemfile.lock

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'action_mailer/railtie'
 require 'active_model/railtie'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
-require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
