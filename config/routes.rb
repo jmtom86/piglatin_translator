53370bcbe996944ade3673b3749a6b89fb27a7e1
Jordan <jmtom86@yahoo.com>
2020-02-17 23:19:18 -0800

    Removed bundler from Gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'pig_latin/index'
   post :'piglatin', to: 'pig_latin#translate'
-
-  root 'pig_latin/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
