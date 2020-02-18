2daabc0d0b7f0de52018148062b6a62382c765e5
Jordan <jmtom86@yahoo.com>
2020-02-17 23:13:48 -0800

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pig_latin/index'
   post :'piglatin', to: 'pig_latin#translate'
+
+  root 'pig_latin/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
